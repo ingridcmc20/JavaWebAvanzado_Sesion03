c90af9bf6d354629398491b9b12bc98f08d1ade3
Alumno <Alumno@LAB701-12>
2018-07-01 11:17:29 -0500

    Sesion 03

---
+++ src/main/java/com/tecsup/javawebavanzado/sesion03/modelo/Categoria.java
@@ -0,0 +1,28 @@
+package com.tecsup.javawebavanzado.sesion03.modelo;
+
+public class Categoria {
+	Integer id;
+	String nombre;
+
+	public Categoria(Integer id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
